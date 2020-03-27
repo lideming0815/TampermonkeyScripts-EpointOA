41db70462c8d775e9efc0be49c8ac097397debde
lidm <375123245@qq.com>
2020-03-27 14:21:53 +0800

    修复日志预选选择问题

--- scripts/日志报销-选择预算方法修改.user.js
+++ scripts/日志报销-选择预算方法修改.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://oa.epoint.com.cn/EpointCBM/Pages/BudgetInfo/NBudgetSelect/BudgetSelectDetail_List.aspx*|
+// @match        https://oa.epoint.com.cn/EpointCBM/Pages/BudgetInfo/NBudgetSelect/BudgetSelectDetail_List.aspx*
 // @grant        none
 // ==/UserScript==
 
